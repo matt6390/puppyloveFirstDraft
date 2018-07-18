e8a21e83bf7c086246ec3f68eabb989abcc8f8ff
matt6390 <matthewstone639@gmail.com>
2018-07-18 00:58:33 -0500

    Cards have placeholder for an Img, will make a way to upload pictures tomorrow

--- public/home.js
+++ public/home.js
@@ -5,7 +5,7 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     document.getElementById("user").innerText =
       "Welcome to Puppy Love " + user.displayName + ".";
-    // console.log(user);
+    console.log(user);
     getUsers();
   } else {
     console.log("No one is signed in...");
@@ -18,12 +18,69 @@ function nextPerson() {
   console.log(PROFILES);
 }
 
-function orderProfiles() {
-  console.log("before loop");
-  for (var i = 3; i <= 0; i--) {
-    PROFILES.push("yo");
+function getProfileInfo(index, key) {
+  index = index - 1;
+  console.log(PROFILES[index][key]);
+  return PROFILES[index][key];
+}
+
+function createProfileCards() {
+  var length = PROFILES.length;
+
+  while (length > 0) {
+    //create card
+    var div = document.createElement("div");
+    div.setAttribute("class", "card");
+
+    //create card picture
+    var img = document.createElement("img");
+    img.setAttribute("class", "card-img-top");
+    img.setAttribute("class", "profile-img");
+    img.setAttribute("class", "img-fluid");
+    img.setAttribute("height", "200");
+    img.setAttribute("width", "200");
+    img.setAttribute(
+      "src",
+      "http://www.irdconline.com/wp-content/uploads/2018/05/person-placeholder.jpg"
+    );
+    div.appendChild(img);
+
+    //create h4 NAME and append it to the card div
+    var h4 = document.createElement("h4");
+    h4.setAttribute("class", "card-title");
+    h4.innerText = getProfileInfo(length, "name");
+    div.appendChild(h4);
+
+    //create ul
+    var ul = document.createElement("ul");
+    ul.setAttribute("class", "card-text");
+    ul.setAttribute("class", "list-group");
+
+    //create age and bio li's
+    var liAge = document.createElement("liAge");
+    liAge.setAttribute("class", "list-group-item");
+    liAge.innerText = getProfileInfo(length, "age");
+
+    var liBio = document.createElement("liBio");
+    liBio.setAttribute("class", "list-group-item");
+    liBio.innerText = getProfileInfo(length, "bio");
+
+    //append li's to the ul
+    ul.appendChild(liAge);
+    ul.appendChild(liBio);
+    div.appendChild(ul);
+
+    //add card to the list
+    var pageUl = document.getElementById("profiles");
+    pageUl.appendChild(div);
+
+    length--;
+    // console.log(div);
   }
-  console.log(PROFILES);
+}
+
+function orderProfiles() {
+  createProfileCards();
 }
 
 function accountExists() {
@@ -81,7 +138,7 @@ function getUsers() {
       // li.setAttribute("class", "profile");
       // ul.appendChild(li);
       // li.innerText = ;
-      console.log(child.val());
+      // console.log(child.val());
     });
     orderProfiles();
   });
